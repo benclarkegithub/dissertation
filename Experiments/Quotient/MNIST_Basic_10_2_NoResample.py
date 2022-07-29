c19feaa02258973a30ca8d049a28f03f5f686d5e
Ben Clarke <benclarke.website@gmail.com>
2022-07-29 02:00:07 +0100

    Ran Quotient "Default" and "NoResample" experiments

--- Experiments/Quotient/MNIST_Basic_10_2_NoResample.py
+++ Experiments/Quotient/MNIST_Basic_10_2_NoResample.py
@@ -1,10 +1,10 @@
 from Datasets import MNIST
 from Evaluate import Evaluate as Eval
 from Methods.Quotient import Quotient
-from Architectures.Basic import Encoder, EncoderToLatents, LatentsToDecoder, Decoder
+from Architectures.Basic import Canvas, Encoder, EncoderToLatents, LatentsToDecoder, Decoder
 
 
-SEED = 2024
+SEED = 2030
 BATCH_SIZE = 128
 TRAIN_SET_SIZE = 57000
 VAL_SET_SIZE = 3000
@@ -13,6 +13,7 @@ mnist = MNIST(
     root="../../Data" , batch_size=BATCH_SIZE, train_set_size=TRAIN_SET_SIZE, val_set_size=VAL_SET_SIZE, seed=SEED)
 
 ARCHITECTURE = {
+    "Canvas": Canvas,
     "Encoder": Encoder,
     "EncoderToLatents": EncoderToLatents,
     "LatentsToDecoder": LatentsToDecoder,
@@ -22,19 +23,22 @@ ARCHITECTURE = {
 NUM_LATENTS = 10
 NUM_LATENTS_GROUP = 2
 
-method = Quotient(architecture=ARCHITECTURE, num_latents=NUM_LATENTS, num_latents_group=NUM_LATENTS_GROUP)
+method = Quotient(
+    architecture=ARCHITECTURE, num_latents=NUM_LATENTS, num_latents_group=NUM_LATENTS_GROUP, variant="NoResample")
 
 MAX_EPOCHS = 100
 MAX_NO_IMPROVEMENT = 25
+GET_GRAD = True
 
-evaluate = Eval(method=method, experiment="MNIST_Basic_10_2", seed=SEED)
+evaluate = Eval(method=method, experiment="MNIST_Basic_10_2_NoResample", seed=SEED)
 
 # Train model
 evaluate.train(
     train_loader=mnist.get_train_loader(),
     val_loader=mnist.get_val_loader(),
     max_epochs=MAX_EPOCHS,
-    max_no_improvement=MAX_NO_IMPROVEMENT)
+    max_no_improvement=MAX_NO_IMPROVEMENT,
+    get_grad=GET_GRAD)
 
 # Test model
 evaluate.test(test_loader=mnist.get_test_loader())
